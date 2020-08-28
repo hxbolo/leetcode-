17154460f03b965f654ea0d11a0c926b1238e097
huangxin <huangxin@doweidu.com>
2020-08-28 15:36:02 +0800

    8.28

--- 2020.08.28.js
+++ 2020.08.28.js
@@ -5,23 +5,20 @@
 // 如果你能让 arr 变得与 target 相同，返回 True；否则，返回 False 。
 // target = [1,2,3,4], arr = [2,4,1,3]
 
-
 /**
  * @param {number[]} target
  * @param {number[]} arr
  * @return {boolean}
  */
-var canBeEqual = function(target, arr) {
-  target.sort()
-  arr.sort()
+var canBeEqual = function (target, arr) {
+  target.sort();
+  arr.sort();
   console.log(JSON.stringify(target) == JSON.stringify(arr));
-
 
   // if(target.toString() === arr.toString())return true
   // return false
 
-  return JSON.stringify(target) == JSON.stringify(arr)
-  
+  return JSON.stringify(target) == JSON.stringify(arr);
 };
 // console.log(canBeEqual([1,2,3,4], [2,4,1,3]));
 
@@ -37,18 +34,14 @@ var canBeEqual = function(target, arr) {
  * @param {number} n
  * @return {number[]}
  */
-var sumZero = function(n) {
-  let arr = []
-  for(let i = 1; i<= n/2; i++) {
-    console.log(i);
-    
-    
-    arr.push(i,-i)
+var sumZero = function (n) {
+  let arr = [];
+  for (let i = 1; i <= n / 2; i++) {
 
+    arr.push(i, -i);
   }
-  if(n%2 !==0) arr.push(0)
+  if (n % 2 !== 0) arr.push(0);
   console.log(arr);
-  return arr
-
+  return arr;
 };
-sumZero(5)
+sumZero(5);
