8670be82930bbbbeea1e12599d8ac99e0f82c02c
huagnxin <964124947@qq.com>
2021-05-07 15:27:18 +0800

    手写new

--- 每天一题面试题/2021.05.07.js
+++ 每天一题面试题/2021.05.07.js
@@ -10,3 +10,48 @@ function add (a, b ){
 }
 
 console.log(add(0.1, 0.2));
+
+
+const a = {
+  b: 2,
+  foo: function () { console.log(this) }
+}
+
+function b(foo) {
+
+  // 输出什么？
+  foo()
+}
+
+b(a.foo)
+
+
+// 手写new 
+
+const argText = function(Fn, ...argument){
+  // 创建对象
+
+  const obj = {}
+  // this指向这构造函数
+  const result = Fn.call(obj, ...argument)
+// 3.如果该构造函数的返回值是Object类型则返回该对象
+  if(result instanceof Object){
+    return result
+  }
+  
+  // 执行构造函数
+  obj.__proto__ = Fn.prototype
+  obj.__proto__.constructor = Fn
+  // 返回这个对象
+
+  return obj
+}
+
+function Person(name, age){
+  this.name = name
+  this.age = age
+}
+
+Person.prototype.type = 'jjj'
+let person =  argText(Person, 'hx','18')
+console.log(person);
